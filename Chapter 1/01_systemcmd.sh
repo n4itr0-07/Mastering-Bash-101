bb3c77c4d4a4efc05f7a20e3c11ac2c84ef401b2
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-11-22 22:31:49 +0530

    Bash Chapter 1 System Commands

---
+++ Chapter 1/01_systemcmd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃🍃"
+
+user_info=$(whoami)
+dir_info=$(pwd)
+date_today=$(date)
+
+echo "Hy NAITRO 07 you are curently logged in as $user_info and your present working directory is $dir_info also today is $date_today !!"
+
+
+echo "and the system uptime is $(uptime)"
+
+
