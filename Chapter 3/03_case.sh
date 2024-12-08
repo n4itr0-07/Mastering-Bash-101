6dad74c5872783d8e86c9af8be830f3459ee5993
Salik Seraj Naik <salikserajnaik@gmail.com>
2024-12-08 22:44:02 +0530

    Chapter 3 | cases in bash 2

--- Chapter 3/03_case.sh
+++ Chapter 3/03_case.sh
@@ -46,3 +46,27 @@ esac
 
 # The case statement is a versatile tool for handling multiple conditions in a script. It can be used to simplify complex logic and improve readability.
 
+# Run the script and enter different numbers to see the output for each case. Try entering values outside the range of 1 to 3 to see the default message.
+
+# The case statement is a powerful alternative to the if statement. It is used to match a value against a list of patterns. The syntax is as follows:
+
+# one more example of case statement in bash script for files and directories is as follows:
+
+echo "Enter a file or directory name"
+
+read name
+
+if [ -f $name ]; then
+    echo "$name is a file."
+elif [ -d $name ]; then
+    echo "$name is a directory."
+else
+    echo "$name is neither a file nor a directory."
+fi
+
+# The case statement can also be used to check if a value is a file, directory, or neither. In this example, the user is prompted to enter a file or directory name. The script checks if the input is a file using the -f flag, a directory using the -d flag, or neither. The appropriate message is displayed based on the result.
+
+# Run the script and enter the name of an existing file or directory to see the output. Try entering a name that does not exist to see the default message.
+
+# The case statement is a versatile tool for handling multiple conditions in a script. It can be used to simplify complex logic and improve readability.
+
